e6cac3831310f448a622bfd257474282f58eb175
david-SPLTJW <david.ortiz11@upr.edu>
2019-10-19 20:58:09 -0400

    twitter image reply bot version 2.0.0

--- twitter-image-reply-bot/reply_bot.py
+++ twitter-image-reply-bot/reply_bot.py
@@ -1,102 +1,96 @@
-#  Author       : David J. Ortiz Rivera / kytrnd / STILLinBL00M
-#  Project      : Twitter Image Reply BOT (https://github.com/kytrnd/Image-Reply-Bot)
-#  File         : img_reply_bot.py
-#  Description  : Replies to a Twitter user with a query-based png album.
-#  Version      : 1.0.0
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+# import api keys
 from setup import *
 
-# id_generator(size,chars) : Creates an id to help differentiate images.
-#                          : The id consists of uppercase ascii caracters and digits.
+#  get_id(img: int): upload image and save media id
+def get_id(img):
+    image = gis.results()[img]
+    # get image as raw data
+    raw_img = image.get_raw_data()
+    data = twitter.upload_media(media=raw_img)
+    ids[img] = data["media_id"]
 
-def id_generator(size=8, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-# img_search(query): Downloads 4 query-based png images to dir_path.
-
+# img_search(query: string): fetch 4 query-based images
 def img_search(query):
-
-    buildargs = {
-        'serviceName': 'customsearch',
-        'version': 'v1',
-        'developerKey': developerKey
-    }
-
-    cseargs = {
-        'q': query,
-        'cx': cx,
-        'num': 4,
-        'searchType': 'image',
-        'fileType': 'png'
-    }
-
-    search.info('Attempt (' + query + ')')
-    results = search_google.api.results(buildargs, cseargs)
-    links = results.links
-    key = id_generator()
+    image_log.info("query attempt {%s}" % (query))
+    # set search parameters
+    search_params = {"q": query,"num": 4,"safe": "off","fileType": "png"}
+    # search images
+    gis.search(search_params=search_params,cache_discovery=False)
+    # fetch media ids using multithreading
+    threads = []
+    # create threads 
+    for img in range(0,len(gis.results())):
+        thread = Thread(target=get_id,args=[img])
+        threads.append(thread)
+        thread.start() 
+    # block until all ids are fetched
+    for thread in threads:
+        thread.join()
     
-    # Images will be saved in this path.
-    dir_path = 'your_path'    
-    if not path.exists(dir_path):
-        makedirs(dir_path)
+    image_log.info("query success {%s}" % (query))
 
-    # Name and save each image. 
-    for i, url in enumerate(links):
-        if 'start' in results.cseargs:
-            i += int(results.cseargs['start'])
-        ext = '.' + results.cseargs['fileType']
-        file_name = results.cseargs['q'].replace(' ', '_') + '_' + str(i) + key + ext
-        file_path = path.join(dir_path, file_name)
-        r = requests.get(url, stream=True)
-        if r.status_code == 200:
-            with open(file_path, 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-    search.info('Success (' + query + ')')
-    return key
-
-# Streamer Class : 
-#   on_success(self,data)           : If @handle is detected in stream, log and respond with 4 png images.
-#   on_error(self,status_code,data) : If some error occurs, log it, print status code, and disconnect the stream.
+# get_query(tweet: string): extract query from tweet by removing all handles
+def get_query(tweet):
+    tweet = tweet.split()
+    for word in tweet:
+        # remove handles
+        if "@" in word:
+            tweet.remove(word)
+    # return query
+    return " ".join(tweet)
 
 class Streamer(TwythonStreamer):
-
+    # on_success(self: streamer,data: json): if handle and query is detected in stream, respond with query album
     def on_success(self,data):
-        if(data['user']['screen_name'] != handle): # Validates input and prevents a reply to itself
-            streamer.info('Attempt (User: ' + data['user']['screen_name'] + 'Query: ' + data['text'] + ')')
-            twitter = Twython(app_key,app_secret,oauth_token,oauth_token_secret)
-            query = data['text'].split()
-            tmp = ''
-            for i in query:
-                if '@' not in i:
-                    tmp += i + ' '
-            tmp = tmp[:-1]
-            
-            reply = '@'+data['user']['screen_name'] + ' ' + tmp
-            key = img_search(tmp)
+        # extract tweet data from response
+        tweet = data["text"]
+        reply_id = data["id_str"]
+        reply_handle = data["user"]["screen_name"]      
+        stream_log.info("query attempt {%s}" % (tweet))
 
-            # Obtaining paths for images to be posted.
-            files = glob.glob('your_path/*' + key + '.png')
-            ids = []
-            
-            # Upload images onto twitter.
-            for i in files:
-                photo = open(i, 'rb')
-                response = twitter.upload_media(media=photo)
-                ids.append(response['media_id'])
-            twitter.update_status(status=reply,in_reply_to_status_id=data['id_str'],media_ids=ids)
-            streamer.info('Success (User: ' + data['user']['screen_name'] + 'Query: ' + data['text'] + ')')
+        # like tweet
+        if "good bot" in tweet.lower():
+            twitter.create_favorite(id=reply_id)
+        # album reply, avoid self-reply
+        if(reply_handle != handle):
+            # extract query from tweet
+            query = get_query(tweet)
+            # avoid null queries
+            if query:
+                img_search(query)
+                reply = ("@%s %s %s" % (reply_handle,query ,"🐸"))
+                # respond with album
+                twitter.update_status(status=reply,in_reply_to_status_id=reply_id,media_ids=ids)
+                stream_log.info("query successful {%s}" % (tweet))
 
+    # on_error(self: streamer,status_code: int,data: json): when non-200 status code is received, disconnect the stream
     def on_error(self,status_code,data):
-        streamer.error(status_code)
+        stream_log.error("error %s",(status_code))
         self.disconnect()
+        # terminate
+        os._exit(-1)
 
-# main() : Starts off the twitter stream (tracking by handle).
-
+# main() : initialize stream, filter by handle
 def main():
-    streamer.info('Streamer started')
-    stream = Streamer(app_key,app_secret,oauth_token,oauth_token_secret)
-    stream.statuses.filter(track=handle)
+    # initialize stream
+    streamer.statuses.filter_async(track=handle)
+    stream_log.info("initialized")
+    # end stream on "enter"
+    input("")
+    # streamer.disconnect()
+    stream_log.info("disconnected")
     return 0
 
-main()
+# initialize instances/stream
+if __name__ == "__main__":
+    # save media ids globally
+    ids = {}
+    # create instances
+    gis = google_images_search.GoogleImagesSearch(google_api_key,cx)
+    twitter = Twython(api_key,api_secret,oauth_token,oauth_token_secret)
+    streamer = Streamer(api_key,api_secret,oauth_token,oauth_token_secret)
+    # initialize stream
+    main()
