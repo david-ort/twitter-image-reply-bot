4ed0834d304d4022d6220f91764986356a6b9929
David John Ortiz Rivera <david.ortiz11@upr.edu>
2019-10-19 21:00:38 -0400

    Update setup.py

--- twitter-image-reply-bot/setup.py
+++ twitter-image-reply-bot/setup.py
@@ -9,14 +9,14 @@ from twython import Twython,TwythonStreamer
 # twitter handle
 handle = "img_reply_bot"    # doesn't require "@"
 # twitter API keys
-api_key = "GTW8xPtwWBqo8LrLXNgfmb8tC"
-api_secret = "5WaLwGmnSgLTVjnVSwrUuJC6qvy8q2MPskCxoFaJFvDbuUgGn8"
-oauth_token = "938172754691584001-lBqCACvL4bFLnnLqcFRWcUBTBwuuHV4"
-oauth_token_secret = "PH0NVLYUYmh8AscMGsbq5p4rVRWXZ0KiZbbeL31c0NEUt"
+api_key = ""
+api_secret = ""
+oauth_token = ""
+oauth_token_secret = ""
 
 # google custom search API keys
-cx = "003155942714465478666:a3phhqg2xgo"
-google_api_key = "AIzaSyDvpeEfdyZZIFqgEQ2Gmw5q5LkJN54CnBQ"
+cx = ""
+google_api_key = ""
 
 # logger format
 date_fmt = "%Y-%m-%d %H:%M:%S"
